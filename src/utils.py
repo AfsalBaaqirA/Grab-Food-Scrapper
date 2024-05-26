531e09fc5b55ddfc7a03c8a6016a44b6ac79aca8
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-05-26 21:57:10 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ def setup_driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
     return driver
 
 def store_data(restaurant_data):
