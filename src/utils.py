5c51dd65040065c513a92dd9d0563615e4527661
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-05-26 21:55:05 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,11 @@ import gzip
 import ndjson
 
 def setup_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
     return driver
 
 def store_data(restaurant_data):
