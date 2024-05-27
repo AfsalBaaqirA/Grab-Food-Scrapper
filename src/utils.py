726de49e1914d5c0b49430eaf0e03713ab5884dd
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-05-27 07:13:33 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import gzip
 import ndjson
-import google_colab_selenium as gs
 
 def setup_driver():
-     driver = gs.Chrome()
-     return driver
+    options = webdriver.EdgeOptions()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+    return driver
 
 def store_data(restaurant_data):
         with gzip.open(f"data/restaurant_data.ndjson.gz", "wt", encoding="utf-8") as f:
